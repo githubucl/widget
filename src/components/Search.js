447c7cd52148d2dca7da168b4b2d09e3dd98dbd5
chris Chen <59293007+githubucl@users.noreply.github.com>
2021-01-21 10:51:19 +1300

    before sugar principle 2day

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,32 +1,59 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-const Search=()=>{
-    const [term,setTerm]=useState('')
+const Search = () => {
+    const [term, setTerm] = useState('programming')
+    const [results, setResults] = useState([])
+    console.log(results);
+    useEffect(() => {
+        if (term) {
+            (async () => {
+                const { data } = await axios.get('https://en.wikipedia.org/w/api.php',
+                    {
+                        params: {
+                            action: 'query',
+                            list: "search",
+                            origin: "*",
+                            format: 'json',
+                            srsearch: term
+                        }
+                    })
+                setResults(data.query.search)
+            })()
+        }
+    }, [term])
 
-    useEffect(()=>{
-        (async ()=>{
-await axios.get('https://en.wikipedia.org/w/api.php',
-{
-params:{
-    action:'query',
-    list:"search",
-    origin:"*",
-    format:'json',
-    srsearch:term
-}
-})
-        })()
-    })
-    return(
-        <div>
-           <div className="ui form">
-            <div className="field">
-                <label>Enter Search Term</label>
-                <input className="input"
-                        onChange={e=>setTerm(e.target.value)}
-                />
+    const renderedResults = results.map(result => {
+        return (
+            <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a className="ui button"
+                        href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                    >GO
+                    </a>
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+                </div>
             </div>
-           </div>
+        )
+    })
+
+    return (
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Search Term</label>
+                    <input className="input"
+                        onChange={e => setTerm(e.target.value)}
+                    />
+                </div>
+            </div>
+            <div className="ui celled list">
+                {renderedResults}
+            </div>
         </div>
     )
 }
